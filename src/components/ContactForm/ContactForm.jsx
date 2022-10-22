00c261afc6a05a0adf97e63e159c743afecb8b16
makslusak <106471649+makslusak@users.noreply.github.com>
2022-10-22 14:26:46 +0300

    Fix bug

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -20,7 +20,6 @@ export class ContactForm extends Component {
   };
 
   render() {
-    const { state } = this.props;
     return (
       <form onSubmit={this.handleSubmit} className={css.form}>
         <label className={css.label} htmlFor="name">
@@ -31,7 +30,7 @@ export class ContactForm extends Component {
           onChange={this.handleInputChange}
           className={css.input}
           type="text"
-          value={state.name}
+          value={this.state.name}
           name="name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -45,7 +44,7 @@ export class ContactForm extends Component {
           placeholder="230-23-23"
           className={css.input}
           type="tel"
-          value={state.number}
+          value={this.state.number}
           name="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
